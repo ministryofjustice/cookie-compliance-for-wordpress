651bd6e09939be8ed8bd48be299f1c1006e7f6d2
Adam <adam.brown@digital.justice.gov.uk>
2019-10-08 16:52:46 +0100

    Rewrite CSS independent of GOV.uk
    Although I would have preferred to use a Gov uk component, the banner is
    still in development according to their component schedule. Initially I
    tried hacking together a CSS framework for the plugin using @imports
    from their node package but in the end, it had compatiblity issues with
    some of our sites that also use gov uk styling, and it was better to
    write my own CSS. I've copied the current Gov uk style and where I can
    imported a bit of gov uk CSS code.

--- includes/pub/partials/cookie-compliance-for-wordpress-page.php
+++ includes/pub/partials/cookie-compliance-for-wordpress-page.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Provide a public-facing view for the plugin
+ * Provide a public-facing view
  *
  * This file is used to markup the public-facing aspects of the plugin.
  *
@@ -15,8 +15,8 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/index.php';
 $matches = preg_grep( '/wp-blog-header.php/', get_included_files() );
 
-$domain_name = $_SERVER['SERVER_NAME'];
-filter_var( $domain_name, FILTER_SANITIZE_URL );
+$domainName = $_SERVER['SERVER_NAME'];
+filter_var( $domainName, FILTER_SANITIZE_URL );
 
 if ( ! empty( $matches ) ) {
 	$abspath = dirname( reset( $matches ) ) . '/';
@@ -26,64 +26,49 @@ if ( ! empty( $matches ) ) {
 get_header();
 ?>
 
-<div id="ccfw-settings-page" class="govuk-width-container">
-	<main class="govuk-main-wrapper">
+<div id="ccfw-settings-page-container">
+	<main class="ccfw-settings-page__wrapper">
+		<div class="ccfw-settings-page__row">
+			<h2><?php echo 'Cookies on ' . $domainName; ?></h2>
+			<p>Cookies are files saved on your phone, tablet or computer when you visit a website. We use cookies to
+				store information about how you use the <?php echo 'Cookies on ' . $domainName; ?> website, such as
+				the pages you visit.</p>
+			<h3>Cookie settings</h3>
+			<p>You can choose which cookies you're happy for us to use.</p>
+			<h3>Cookies that measure website use</h3>
+			<p>We use Google Analytics to measure how you use the website so we can improve it based on user needs.
+				Google Analytics sets cookies that store anonymised information about:
+				<ul>
+					<li>how you got to the site</li>
+					<li>the pages you visit on <?php echo 'Cookies on ' . $domainName; ?> and how long you spend on
+						each page</li>
+					<li>what you click on while you're visiting the site</li>
+				</ul>
+			</p>
+			<p>We do not allow Google to use or share the data about how you use this site.</p>
 
-		<div class="govuk-grid-row">
-			<div class="govuk-grid-column-two-thirds">
-
-				<h2 class="govuk-heading-xl"><?php echo 'Cookies on ' . $domain_name; ?></h2>
-				<p>Cookies are files saved on your phone, tablet or computer when you visit a website. We use cookies to
-					store information about how you use the <?php echo 'Cookies on ' . $domain_name; ?> website, such as
-					the pages you visit.</p>
-
-				<h3 class="govuk-heading-l">Cookie settings</h3>
-
-				<p>You can choose which cookies you're happy for us to use.</p>
-
-				<h3 class="govuk-heading-l">Cookies that measure website use</h3>
-				<p>We use Google Analytics to measure how you use the website so we can improve it based on user needs.
-					Google Analytics sets cookies that store anonymised information about:
-
-					<ul>
-						<li>how you got to the site</li>
-						<li>the pages you visit on <?php echo 'Cookies on ' . $domain_name; ?> and how long you spend on
-							each page</li>
-						<li>what you click on while you're visiting the site</li>
-					</ul>
-
-				</p>
-				<p>We do not allow Google to use or share the data about how you use this site.</p>
-
-				<div id="ccfw-settings" class="govuk-radios">
-
-					<div class="govuk-radios__item">
-
-						<input class="govuk-radios__input" id="ga-yes" name="gAnalytics" type="radio" value="" checked>
-						<label class="govuk-label govuk-radios__label" for="ga-yes">
-							Yes
-						</label>
-					</div>
-
-					<div class="govuk-radios__item">
-
-						<input class="govuk-radios__input" id="ga-no" name="gAnalytics" type="radio" value="">
-						<label class="govuk-label govuk-radios__label" for="ga-no">
-							No
-						</label>
-					</div>
-
-					<br><br>
-
-					<h2 class="govuk-heading-l">Strictly necessary cookies</h2>
-
-					<p>In some instances we may use WordPress cookes. These essential cookies do things like:</p>
-					<ul>
-						<li>remember the notifications you've seen so we do not show them to you again</li>
-						<li>allow for interactive website elements</li>
-					</ul>
-					<p>They always need to be on.</p>
-
+			<div class="ccfw-radios ccfw-radios--inline">
+				<div class="ccfw-radios__item">
+					<input class="ccfw-radios__input" id="ga-yes" name="gAnalytics" type="radio" value="" checked>
+					<label class="ccfw-label ccfw-radios__label" for="ga-yes">
+						<?php _e( 'Yes', 'cookie-compliance-for-wordpress'); ?>
+					</label>
+				</div>
+				<div class="ccfw-radios__item">
+					<input class="ccfw-radios__input" id="ga-no" name="gAnalytics" type="radio" value="">
+					<label class="ccfw-label ccfw-radios__label" for="ga-no">
+						<?php _e( 'No', 'cookie-compliance-for-wordpress'); ?>
+					</label>
+				</div>
+			</div>
+			
+			<h2>Strictly necessary cookies</h2>
+			<p>In some instances we may use WordPress cookes. These essential cookies do things like:</p>
+			<ul>
+				<li>remember the notifications you've seen so we do not show them to you again</li>
+				<li>allow for interactive website elements</li>
+			</ul>
+			<p>They always need to be on.</p>
 	</main>
 </div>
 
