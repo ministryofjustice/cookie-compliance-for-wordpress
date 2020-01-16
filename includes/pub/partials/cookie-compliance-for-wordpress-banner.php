84f7cd1c03e9e28a3a7cfc2875f34ac525477dd8
adam <Snail mail>
2020-01-16 20:27:46 +0000

    Update banner to use latest Gov UK text.

--- includes/pub/partials/cookie-compliance-for-wordpress-banner.php
+++ includes/pub/partials/cookie-compliance-for-wordpress-banner.php
@@ -15,21 +15,26 @@ filter_var( $domainName, FILTER_SANITIZE_URL )
 ?>
 
 <div id="ccfw-page-banner-container">
-	<div class="ccfw-banner__wrapper">
-		<p class="ccfw-banner__message"><?php _e( strtoupper( $domainName ) . ' uses cookies which are essential
-			for the site to work. We also use non-essential cookies to help us improve our digital services. Any
-			data collected is anonymised. By continuing to use this site, you agree to our use of cookies.', 'cookie-compliance-for-wordpress'); ?></p>
-		<div class="ccfw-banner-button__wrapper">
-			<div class="ccfw-banner-button">
-				<button type="submit">
-					<?php _e( 'Accept cookies', 'cookie-compliance-for-wordpress' ); ?>
-				</button>
-			</div>
-			<div class="ccfw-banner-button">
-				<a role="button" href="/ccfw-cookie-policy">
-					<?php _e( 'Cookie settings', 'cookie-compliance-for-wordpress' ); ?>
-				</a>
-			</div>
-		</div>
-	</div>
+    <div class="ccfw-banner__wrapper">
+        <span class="ccfw-banner__heading">Tell us whether you accept cookies</span>
+
+        <p class="ccfw-banner__message">
+			<?php _e( 'We use cookies to collect information about how you use ' . strtoupper( $domainName ) . ' . 
+			We use this information to make the website work as well as possible and improve our digital services.', 'cookie-compliance-for-wordpress'); ?>
+        </p>
+
+        <div class="ccfw-banner-button__wrapper">
+            <div class="ccfw-banner-button">
+                <button type="submit">
+                    <?php _e( 'Accept all cookies', 'cookie-compliance-for-wordpress' ); ?>
+                </button>
+            </div>
+            <div class="ccfw-banner-button">
+                <a role="button" href="/ccfw-cookie-policy">
+                    <?php _e( 'Set cookie preferences', 'cookie-compliance-for-wordpress' ); ?>
+                </a>
+            </div>
+        </div>
+		
+    </div>
 </div>
