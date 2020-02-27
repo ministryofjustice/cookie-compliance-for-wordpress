fcd6755ea5148e72b19fd5ff2d9e127b8f149001
adam <adam.brown@digital.justice.gov.uk>
2020-02-27 17:24:43 +0000

    Remove unused css

--- includes/pub/enqueue.php
+++ includes/pub/enqueue.php
@@ -16,21 +16,22 @@ use \CCFW\Controller;
 
 class Enqueue extends Controller
 {
+    
+    public $cssFile;
 
     public function register()
     {
         add_action('wp_enqueue_scripts', array( $this, 'enqueue' ), 11);
-        add_action('admin_enqueue_scripts', array( $this, 'enqueueAdmin' ), 11);
+
+        $this->cssFile = $this->plugin_path . 'dist/css/cookie-compliance-for-wordpress.css';
     }
 
     public function enqueue()
     {
-        wp_enqueue_style('CCFWPluginStyle', $this->plugin_url . 'dist/css/cookie-compliance-for-wordpress.css');
-        wp_enqueue_script('CCFWPluginScript', $this->plugin_url . 'dist/js/cookie-compliance-for-wordpress.js', array( 'jquery' ), '1.0.0', false);
-    }
+        // Cache bust
+        $hash = filemtime($this->cssFile);
 
-    public function enqueueAdmin()
-    {
-        wp_enqueue_style('CCFWPluginStyleAdmin', $this->plugin_url . 'dist/css/admin-cookie-compliance-for-wordpress.css');
+        wp_enqueue_style('CCFWPluginStyle', $this->plugin_url . 'dist/css/cookie-compliance-for-wordpress.css', null, $hash, false);
+        wp_enqueue_script('CCFWPluginScript', $this->plugin_url . 'dist/js/cookie-compliance-for-wordpress.js', array( 'jquery' ), '1.0.0', false);
     }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,5 +4,4 @@ var _src = './src/'
 
 mix_.setPublicPath('./dist')
   .js(_src + 'js/frontend.js', 'js/cookie-compliance-for-wordpress.js')
-  .sass(_src + 'scss/backend-styles.scss', 'dist/css/admin-cookie-compliance-for-wordpress.css')
   .sass(_src + 'scss/frontend-styles.scss', 'dist/css/cookie-compliance-for-wordpress.css')
