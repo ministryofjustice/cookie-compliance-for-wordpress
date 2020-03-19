0fefa17ce8bb3466f18d167e8c4e6d169f4ab5c6
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-03-19 12:52:41 +0000

    Remove Hotjar text

--- includes/pub/partials/components/c-google.php
+++ includes/pub/partials/components/c-google.php
@@ -5,9 +5,9 @@
  */
 
 ?>
-<h2>Google Analytics and Hotjar</h2>
-<p>We use Google Analytics and Hotjar to measure how you use the website so we can improve it based on user needs.
-    Google Analytics and Hotjar set cookies that store anonymised information about:
+<h2>Google Analytics</h2>
+<p>We use Google Analytics to measure how you use the website so we can improve it based on user needs.
+    Google Analytics set cookies that store anonymised information about:
     <ul>
         <li>how you got to the site</li>
         <li>the pages you visit on <?php echo 'Cookies on ' . $domainName; ?> and how long you spend on
@@ -15,7 +15,7 @@
         <li>what you click on while you're visiting the site</li>
     </ul>
 </p>
-<p>We do not allow Google or Hotjar to use or share the data about how you use this site.</p>
+<p>We do not allow Google to use or share the data about how you use this site.</p>
 
 <div class="ccfw-radios ccfw-radios--inline">
     <div class="ccfw-radios__item">
