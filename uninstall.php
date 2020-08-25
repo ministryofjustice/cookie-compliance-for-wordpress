196e8a5d4f6127bbd9048df097da0065ebf1c7bf
adam <adam.brown@digital.justice.gov.uk>
2020-08-25 10:14:24 +0100

    Add uninstall option function for plugin

---
+++ uninstall.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Tasks to carry out on uninstall
+ * https://developer.wordpress.org/plugins/plugin-basics/uninstall-methods/
+ *
+ * @link       https://github.com/ministryofjustice/cookie-compliance-for-wordpress
+ * @since      1.0.0
+ *
+ * @package    cookie-compliance-for-wordpress
+ */
+
+defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
+
+// delete the registered database options
+delete_option('ccfw_plugin_settings');
