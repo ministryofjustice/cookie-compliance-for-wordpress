cf28261a219cecbf570b299510e5b948d3735f7b
Beverley Newing <bnewing92@hotmail.co.uk>
2020-09-22 16:37:46 +0100

    Add ARIA to cookie modal
    The DAC report flagged that the modal wasn't marked up with the appropriate ARIA, and signposted to the W3C example. This adds in the attributes that that resource recommends.

--- components/Banner/partials/partial-banner.php
+++ components/Banner/partials/partial-banner.php
@@ -20,10 +20,10 @@ $bannerTitle = !empty($options['banner_title']) ? $options['banner_title'] : 'Ar
 ?>
 
 <div class="ccfw-background-grey-overlay"></div>
-<div id="ccfw-page-banner">
+<div id="ccfw-page-banner" role="dialog" aria-labelledby="ccfw-banner-title" aria-modal="true">
     <div class="ccfw-banner">
         <div class="ccfw-banner__intro">
-            <?php _e('<h2 class="ccfw-banner__heading">' . esc_attr($bannerTitle) . '</h2>', 'cookie-compliance-for-wordpress'); ?>
+            <?php _e('<h2 class="ccfw-banner__heading" id="ccfw-banner-title">' . esc_attr($bannerTitle) . '</h2>', 'cookie-compliance-for-wordpress'); ?>
             <p class="ccfw-banner__info-text">
                 <?php _e('We use small files called ‘cookies’ on ' . strtoupper($domainName) . ' to give you the best experience on our site.  Some are essential to make the site work, and some help us understand how people use the site so that we can improve your experience. You can choose to turn off the non-essential cookies.  Which cookies are you happy for us to use?', 'cookie-compliance-for-wordpress'); ?>
             </p>
