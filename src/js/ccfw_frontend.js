2cf9481621a742d9cdd6c6f5429cf14767d11883
Beverley Newing <bnewing92@hotmail.co.uk>
2020-11-24 17:35:21 +0000

    Add in helper function that toggles aria-hidden state
    This helper function gets sibling elements and toggles the aria-hidden attribute on them. We're adding this in because we need to add aria-hidden to prevent screen readers getting lost away from the banner, before making a change (based on DAC's recommendation).

--- src/js/ccfw_frontend.js
+++ src/js/ccfw_frontend.js
@@ -68,9 +68,11 @@
       showBanner: function () {
         this.$el.show()
         this.$cookieSettingsButton.hide()
+        this.toggleAriaHidden(this.$el, "hide")
       },
       hideBanner: function () {
         this.$el.hide()
+        this.toggleAriaHidden(this.$el, "unhide")
         this.$cookieSettingsButton.show()
       },
       hideSettingsModal: function () {
@@ -99,6 +101,29 @@
         } else {
           settingsModal.$gaToggleOffText.show()
           settingsModal.$gaToggleOnText.hide()
+        }
+      },
+      toggleAriaHidden: function (elem, state) {
+        var siblings = [];
+        var sibling = elem.parent().children();
+
+        // Loop through each sibling and push to the array
+        while (sibling) {
+          if (sibling !== elem) {
+            siblings.push(sibling);
+          }
+          sibling = sibling.nextSibling
+        }
+
+        if (state === "hide") {
+          siblings.forEach(function (sibling) {
+            sibling.attr("aria-hidden", "true")
+            elem.removeAttr("aria-hidden", "true")
+          })
+        } else {
+          siblings.forEach(function (sibling) {
+            sibling.removeAttr("aria-hidden", "true")
+          })
         }
       }
     }
