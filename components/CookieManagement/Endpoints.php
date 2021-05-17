b4cfc1f89899ae49b98f1bfb1744892d6a5d2e40
wilson1000-MoJ <wilson1000-MoJ@users.noreply.github.com>
2021-05-17 10:08:54 +0000

    Run automated code lint

--- components/CookieManagement/Endpoints.php
+++ components/CookieManagement/Endpoints.php
@@ -4,8 +4,9 @@ namespace CCFW\Components\CookieManagement;
 
 class Endpoints
 {
-    public static function phrase() {
+    public static function phrase()
+    {
         // rest_ensure_response() wraps the data we want to return into a WP_REST_Response, and ensures it will be properly returned.
-        return rest_ensure_response( 'Hello World, this is the WordPress REST API' );
+        return rest_ensure_response('Hello World, this is the WordPress REST API');
     }
 }
